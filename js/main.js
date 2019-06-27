ec92089e2da26125daff7c5d3be86d7c9f46211b
bumblebee <novikovpm3@gmail.com>
2019-06-28 01:07:18 +0300

    Добавляет функции: редомное число, вывод объекта, вывод фотографий, нпм сборку,

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
-var numberPhoto = 1;
-var numberLiks = 1;
-var commentsMassive = [
+'use strict'
+
+
+
+var comments = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
@@ -8,18 +10,29 @@ var commentsMassive = [
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
 ];
-var
 
-var objektPhoto = {
-  url = photos/numberPhoto.jpg,
-  likes = numberLikes,
-  comments =
-
+var getRandomIntager = function (min, max) {
+  return Math.random() * (max - min) + min;
 };
 
-var makeMassivPfoto = function () {
+var getPhoto = function (number) {
+  return {
+    url: 'photos/' + number + '.jpg',
+    likes: getRandomIntager(15, 201),
+    comments: getRandomIntager (comments[0], comments.length - 1)
+  };
+};
+
+var getPhotos = function () {
+  var array = [];
   for (var i = 1; i <= 25; i++) {
-    var callObject =
+    array.push(getPhoto(i));
   }
+  return array;
+};
+
+var photos = getPhotos();
+
+var renderPhotos = function (array) {
 
 };
